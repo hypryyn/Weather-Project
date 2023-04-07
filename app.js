826ddf078c6675b6ec02e699a19cde4871fa5413
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-07 22:43:13 +0700

    adding city input

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
 
-    let city = "Tumpang"
+app.post("/", function (req, res) {
+    let query = req.body.cityName;
     const APIkey = "505481f86e7738cdb25b64968517079b"
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + APIkey
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + APIkey
 
     https.get(url, function (response) {
         console.log(response.statusCode);
@@ -17,12 +24,15 @@ app.get("/", function (req, res) {
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             const weatherDescription = weatherData.weather[0].description
-            console.log(weatherDescription);
-            console.log(temp);
+            const icon = weatherData.weather[0].icon
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<p>The weather is currently " + weatherDescription + "<p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degree Celcius.</h1>");
+            res.write("<img src=" + imageURL +">");
+
+            res.send()
         })
     })
-
-    res.send("Server is up and running.")
 })
 
 
